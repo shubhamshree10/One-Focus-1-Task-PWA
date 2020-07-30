bd15d6424f08d69f3f1d43c48ba60c7490a52501
Shubham <shubhamshree10@gmail.com>
2020-07-31 00:22:15 +0530

    Added FB redirect to my ownn page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,27 @@
 // Initialize the FirebaseUI Widget using Firebase.
+    
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '{2463169880640837}',
+        cookie     : true,
+        xfbml      : true,
+        version    : '{v7.0}'
+      });
+        
+      FB.AppEvents.logPageView();   
+        
+    };
+  
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  
+
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start('#firebaseui-auth-container', {
